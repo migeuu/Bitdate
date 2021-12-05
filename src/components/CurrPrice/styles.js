7b517ded47e0a86276ff72882cb8515025e97417
migeuu <miguelmarkiori@gmail.com>
2021-12-05 11:31:29 -0300

    styles CurrPrice

---
+++ src/components/CurrPrice/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  headerPrice: {
+    width: "100%",
+    height: "auto",
+    alignItems: "center",
+  },
+  currPrice: {
+    color: "#daa520",
+    fontSize: 32,
+    fontWeight: "bold",
+    paddingTop: 25,
+  },
+  textPrice: {
+    color: "#ffffe6",
+    marginBottom: 16,
+    fontSize: 12,
+    letterSpacing: 0.7,
+    fontWeight: "bold",
+  },
+});
