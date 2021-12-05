a7d5410d596fe8769221f6778898cc35dab12fe2
migeuu <miguelmarkiori@gmail.com>
2021-12-05 11:32:14 -0300

    filterQuerry styles

---
+++ src/components/QuotationsList/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  filters: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    paddingVertical: 15,
+  },
+  buttonQuery: {
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    borderWidth: 1.4,
+    borderColor: "#daa520",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textButtonQuery: {
+    color: "#ffffe6",
+    fontWeight: "bold",
+  },
+  QuotationContext: {
+    width: "90%",
+  },
+});
